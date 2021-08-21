fb74f0331bfde96a1c1311132462951fe2973fa7
Bertrik Sikken <bertrik@sikken.nl>
2021-08-21 11:57:24 +0200

    Remove unused test

--- noiseforwarder/src/test/java/nl/bertriksikken/noiseforwarder/noise/NoiseMessageTest.java
+++ noiseforwarder/src/test/java/nl/bertriksikken/noiseforwarder/noise/NoiseMessageTest.java
@@ -7,23 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public final class NoiseMessageTest {
-
-    /**
-     * Verifies parsing from raw byte array.
-     */
-//    @Test
-//    @Ignore
-//    public void testParseRaw() throws NoiseParseException {
-//        byte[] data = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x00,
-//                0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x00, 0x11, 0x22, 0x33 };
-//
-//        NoiseMessage message = NoiseMessage.parse(data);
-//        Assert.assertArrayEquals(new double[] { 27.4, 56.3, 109.3 }, message.getLa(), 0.1);
-//    }
 
     /**
      * Verifies parsing from JSON data.
